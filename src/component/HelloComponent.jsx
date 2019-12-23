968f087d93622829a7219c560ec2f6071c645d0b
doet <dit_doet@yahoo.com>
2019-12-23 10:12:50 +0700

    step pra 1

---
+++ src/component/HelloComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './HelloComponent.css';
+
+const HelloComponent = () => {
+  return <p className='text-p'>Text Warna Merah Component</p>
+}
+
+export default HelloComponent;
---
+++ src/container/StateFullComponet.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './StateFullComponet.css';
+
+class StateFullComponet extends React.Component {
+ render(){
+   return <p className='text-p'>StateFull Componet</p>
+ }
+}
+
+export default StateFullComponet;
